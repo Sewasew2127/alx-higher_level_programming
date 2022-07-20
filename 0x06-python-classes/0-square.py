20ba007a0aa758595064e6fe064dbcad380851bf
Sewasew <emo21.sew@gmail.com>
2022-07-20 10:38:13 +0300

    python documetnation added

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-
+"""
+empty class for Square that defines a square
+imporing any module is not allowed
+"""
 
 class Square:
     """
-    empty class for Square
+    empty method for Square that defines a square
+    imporing any module is not allowed
     """
     pass
+
