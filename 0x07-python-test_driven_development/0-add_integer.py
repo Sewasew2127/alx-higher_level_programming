96d3d8b91bc20d096f946b7407998e57d2f5c01a
Sewasew <emo21.sew@gmail.com>
2022-08-09 09:53:38 +0300

    python

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-
-"""Return the sum
-a and b must be integers or floats,
-otherwise raise a TypeError exception
-a and b must be first casted to integers if they are float
-"""
+# 0-add_integer.py
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """adds an integer
-    unit tests located in tests/0-add_integer.txt
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, int):
-        if isinstance(a, float):
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        if isinstance(b, float):
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return a + b
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
