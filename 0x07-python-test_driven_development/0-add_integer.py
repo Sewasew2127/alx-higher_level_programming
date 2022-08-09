aa3e31daa40091d15a98fed3c8ee278a42e40261
Sewasew <emo21.sew@gmail.com>
2022-08-09 09:43:08 +0300

    pycodestyle is fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,12 +2,12 @@
 
 """
 function: add_integer
-arguments: 2
-    a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
-    a and b must be first casted to integers if they are float
-unit tests located in tests/0-add_integer.txt
-checks for type errors
-"""
+a and b must be integers or floats,
+otherwise raise a TypeError exception with
+the message a must be an integer or
+b must be an integer
+a and b must be first casted to integers if they are float
+unit tests located in tests/0-add_integer.txt"""
 
 
 def add_integer(a, b=98):
