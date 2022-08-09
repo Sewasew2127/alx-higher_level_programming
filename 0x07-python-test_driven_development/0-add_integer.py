cab21c0d9a6ed38f87bd979f864cea129aaea625
Sewasew <emo21.sew@gmail.com>
2022-08-09 09:47:15 +0300

    documetation is fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 
-"""
-function: add_integer
+"""Return the sum
 a and b must be integers or floats,
-otherwise raise a TypeError exception with
-the message a must be an integer or
-b must be an integer
+otherwise raise a TypeError exception
 a and b must be first casted to integers if they are float
-unit tests located in tests/0-add_integer.txt"""
+"""
 
 
 def add_integer(a, b=98):
-    """
-    adds an integer
+    """adds an integer
     unit tests located in tests/0-add_integer.txt
-    checks for type errors
     """
     if not isinstance(a, int):
         if isinstance(a, float):
