f256a4a5024a52f4b653ae4d5b2c04d7246658a8
Sewasew <emo21.sew@gmail.com>
2022-08-31 09:39:03 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+Protototype: def read_file(filename)
+
+"""
+
 
 def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8)
+    and prints it to stdout
+    use the with statement
+     """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
