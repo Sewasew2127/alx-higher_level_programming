da32132474cb88941941ec23e9d888b1e9ec2f74
Sewasew <emo21.sew@gmail.com>
2022-08-31 10:24:05 +0300

    advanced task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+*******************************
+Supproting file:
+to run:
+a script that adds all arguments to a Python list,
+and then save them to a file
+*******************************
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts text after each line containing
+    a given string in a file.
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search
+        for within the file.
+        new_string (str): The string to insert.
+
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
