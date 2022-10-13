7bddfce87cfada989e3a3c0edb9cda577c203c07
Sewasew <emo21.sew@gmail.com>
2022-10-13 10:03:33 +0300

    Utput spelling solved

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arrOfSt = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrOfSt = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < arrOfSt.length; i++) {
   console.log(arrOfSt[i]);
 }
