7635989a3e8f6f59e2e68f76a8c11d5e2af71617
Sewasew <emo21.sew@gmail.com>
2022-10-19 16:19:39 +0300

    fixing 2 tasks

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 --a script to create a database and a user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- query to grant permissions to new user
 GRANT USAGE ON * . * TO 'user_0d_2'@'localhost';
 GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,5 @@
 --a script to create a table id_not_null
+-- trying to do sth here
 CREATE TABLE IF NOT EXISTS id_not_null(
   id INT DEFAULT 1,
   name VARCHAR(256)
