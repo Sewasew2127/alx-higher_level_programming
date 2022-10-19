6d7d39424e6db457ff85edcc71b7fcf805bad4e8
Sewasew <emo21.sew@gmail.com>
2022-10-19 16:05:37 +0300

    task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script tp create MYSQL server user called user_od_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---granting privilages
+-- query to grant perms to user
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
