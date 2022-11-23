521989f1a918ae77c4a55ce3d25ba287a83e4905
Sewasew <emo.sew@gmail.com>
2022-11-23 09:53:18 +0300

    fixed the errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+// A script to print the title of a star wars movie
+let process = require('process')
+let args = process.argv.slice(2)
+let request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + args[0], function(err, res, body){
+  //if (err) throw err;
+  //res.stringify();
+  console.log(JSON.parse(body).title)
 });
